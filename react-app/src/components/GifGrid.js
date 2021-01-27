e04df986dfed24d8266c4ae58ecb4800a308ffc3
Andrés Arana Castillo <aac.devs@gmail.com>
2021-01-27 11:13:27 -0300

    CustomHook useFetchGifs created

--- react-app/src/components/GifGrid.js
+++ react-app/src/components/GifGrid.js
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifGridItem } from "./GifGridItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs(category).then(setImages);
-  }, [category]);
-
+  const { data: images, loading } = useFetchGifs(category);
   return (
     <>
       <h3>{category}</h3>
+      {loading && <p>Loading..</p>}
       <div className="card-grid">
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />
