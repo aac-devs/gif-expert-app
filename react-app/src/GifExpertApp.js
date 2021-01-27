c89fb38a6c13ccaa47c13bab65828082ef724d63
Andrés Arana Castillo <aac.devs@gmail.com>
2021-01-27 10:05:51 -0300

    GifGrid component created

--- react-app/src/GifExpertApp.js
+++ react-app/src/GifExpertApp.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState([
-    "One Punch",
-    "Samurai X",
-    "Dragon Ball",
-  ]);
-
-  // const handleAdd = () => {
-  //   // setCategories([...categories, "Supercampeones"]);
-  //   setCategories((cats) => ["Supercampeones", ...cats]);
-  // };
+  const [categories, setCategories] = useState(["One Punch"]);
 
   return (
     <>
@@ -20,7 +12,7 @@ export const GifExpertApp = () => {
       <hr />
       <ol>
         {categories.map((category) => (
-          <li key={category}>{category}</li>
+          <GifGrid key={category} category={category} />
         ))}
       </ol>
     </>
